7d2d84e6bc6bc9951175df6b500ac5c6d0838222
Alpha-Cyg <124375153+Alpha-Cyg@users.noreply.github.com>
2023-02-06 15:13:54 +0800

    learning record

---
+++ test_1_25/test_1_25/test.c
@@ -0,0 +1,27 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+//int main()
+//{
+//	printf("hello world");
+//	return 0;
+//
+//}
+//int main()
+//{
+//	int num1=0, num2=0;
+//	puts("请输入要进行求和的两个整数");
+//
+//	printf("整数1：");// \n 为换行符号
+//	scanf("%d", &num1);
+//	printf("整数2：");
+//	scanf("%d", &num2);
+//	printf("它们的和为%d.\n", num1 + num2);
+//
+//	return 0;
+//}
+int main()
+{
+	printf("%d",0123+8775);
+	return 0;
+}
